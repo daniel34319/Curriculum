23d89e836a8cf0387ddd5ae72bcec0391a67e724
daniel34319 <123826820+daniel34319@users.noreply.github.com>
2023-01-31 10:00:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function alerta() {
+    alert("Gracias por ver mi Curriculum");
+}
+
+function redes() {
+    document.getElementById("demo").innerHTML = "facebook.com/wayne instagram.com/wayne"
+}
+
+function detalles() {
+    document.getElementById("demo").innerHTML = "Permiso para conducir A3, B1 / Disponibilidad Horaria Full Time"
+}
